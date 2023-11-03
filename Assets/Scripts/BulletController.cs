443ffabb603b64b20d34241c2bcd44b78e4d0b23
anicolauiesmanacor <anicolau@politecnicllevant.cat>
2023-11-03 16:41:31 +0100

    a
    a

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -3,9 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletController : MonoBehaviour {
+    [SerializeField] private AudioClip hit;
+    AudioSource fxSource;
+
+    void Start() {
+        fxSource = GameObject.FindGameObjectWithTag("FX_AudioSource").GetComponent<AudioSource>();
+    }
+
     private void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.CompareTag("Target")) {
             print("Hit: " + collision.gameObject.name + "!");
+            fxSource.clip = hit;
+            fxSource.Play();
             Destroy(gameObject);
         } else if (collision.gameObject.CompareTag("Wall") || collision.gameObject.CompareTag("Ground")) {
           Destroy(gameObject);
